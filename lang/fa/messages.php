b52662f66d880c093d04c7728c1fb9db441328aa
m.alizadeh <m.alizadeh@ip-jam.com>
2023-02-28 11:33:55 +0330

    changed connection timeout message

--- lang/fa/messages.php
+++ lang/fa/messages.php
@@ -3,5 +3,5 @@
 return [
     'invalid_token'      => 'توکن ارسالی نامعتبر می باشد.',
     'invalid_user'       => 'نام کاربری شما در این سامانه تعریف نشده است.',
-    'connection_timeout' => 'درحال حاضر سرور SSO پاسخگو نیست.'
+    'connection_timeout' => 'درحال حاضر سرویس SSO پاسخگو نیست.'
 ];
