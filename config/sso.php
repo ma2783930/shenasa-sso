c2257c9c334a4d58f2cda9f909cf6de9f624f26c
m.alizadeh <m.alizadeh@ip-jam.com>
2023-05-10 19:39:39 +0330

    sso routes now are static

--- config/sso.php
+++ config/sso.php
@@ -5,8 +5,5 @@ return [
     'base_address'    => env('SSO_BASE_ADDRESS'),
     'client_id'       => env('SSO_CLIENT_ID'),
     'client_secret'   => env('SSO_CLIENT_SECRET'),
-    'login_url'       => env('SSO_LOGIN_URL', 'sso-login'),
-    'logout_url'      => env('SSO_LOGOUT_URL', 'sso-logout'),
-    'callback_url'    => env('SSO_CALLBACK_URL', 'sso-callback'),
     'auth_middleware' => env('SSO_AUTH_GUARD', 'auth:sanctum')
 ];
--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,13 @@ use Shenasa\Facades\Sso;
 use Shenasa\Http\Controllers\SsoAuthController;
 
 if (Sso::getIsEnable()) {
-    Route::middleware('web')->group(function(){
-        Route::middleware('guest')->group(function(){
-            Route::get(config('sso.login_url'), [SsoAuthController::class, 'login'])->name('sso-auth.login');
-            Route::get(config('sso.callback_url'), [SsoAuthController::class, 'callback'])->name('sso-auth.callback');
+    Route::prefix('sso')->middleware('web')->group(function () {
+        Route::middleware('guest')->group(function () {
+            Route::get('login', [SsoAuthController::class, 'login'])->name('sso-auth.login');
+            Route::get('callback', [SsoAuthController::class, 'callback'])->name('sso-auth.callback');
         });
-        Route::middleware(config('sso.auth_middleware'))->group(function(){
-            Route::get(config('sso.logout_url'), [SsoAuthController::class, 'logout'])->name('sso-auth.logout');
+        Route::middleware(config('sso.auth_middleware'))->group(function () {
+            Route::get('logout', [SsoAuthController::class, 'logout'])->name('sso-auth.logout');
         });
     });
 }
